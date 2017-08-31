793d914a87a9f17f9fd3adc19f7f2f4fe3815c13
Mark <mark.sarl@gmail.com>
2017-08-31 15:27:13 +0100

    added eslint

--- imports/math.js
+++ imports/math.js
@@ -2,7 +2,7 @@ export default add = function (a, b) {
     if (typeof(a)=== 'number' && typeof(b)==='number'){
         return a + b;
     } else {
-        return 'Invalid parameters';
+        return 'Invalid parameters'
     }
     
 };
