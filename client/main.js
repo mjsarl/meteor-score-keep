204b6b47fb6eb773dda0ac561c0676bb94986f3d
Mark <mark.sarl@gmail.com>
2017-08-31 15:00:06 +0100

    Split out name log

--- client/main.js
+++ client/main.js
@@ -2,7 +2,9 @@ import {greetUser, name} from './../imports/utils';
 import defaultFun from './../imports/math';
 
 console.log('Log from /client/main.js');
-console.log(greetUser(), name);
+console.log(greetUser());
+console.log(name);
+
 console.log('10 + 2 = ', defaultFun(10,2));
 
 
